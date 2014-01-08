3ab6fb06dc6827d2174560aa3eed8b52951b3ac3
= <arun.n@railsfactory.org>
2014-01-08 17:47:28 +0530

    intial app code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KendoUi::Application.config.secret_key_base = '090fdfc6e15767c0221c5882ad28f75f0bf73309756d022fc5fd1a6d368e984caed204029a637d991d976b8904fd9c3cbf79ae9c9b3ea417e306bcd28113e6f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KendoUi::Application.config.session_store :cookie_store, key: '_kendo_ui_session'
